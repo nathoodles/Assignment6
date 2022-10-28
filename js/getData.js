bfdd4c29c7a51ced2b2498a06ab875788bf048a6
nathoodles <114275088+nathoodles@users.noreply.github.com>
2022-10-28 11:08:12 -0700

    Add files via upload

---
+++ js/getData.js
@@ -0,0 +1,22 @@
+$.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+    var number = data['number'];
+    $('#SpacePeople').html(number);
+
+    let info = Array();
+    data['people'].forEach(function (d) {
+        info.push([d['craft'], d['name']]);
+    });
+
+    var last = info[0][0];
+    $('#craftNames').append('<dt>' + info[0][0] + ': </dt>');
+    for(let i = 0; i<info.length; i++){
+        if(info[i][0] == last){
+            $('#craftNames').append('<dd>' + info[i][1] + '</dd>');
+        }else{
+            $('#craftNames').append('<dt>' + info[i][0] + ': </dt>');
+            $('#craftNames').append('<dd>' + info[i][1] + '</dd>');
+            last = info[i][0];
+        }
+    }
+
+});
